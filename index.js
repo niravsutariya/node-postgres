823dcefed5d614db7a13ff71c1b610b4cf15047b
Nirav Sutariya <sutariya_nirav@yahoo.com>
2021-03-10 11:54:32 +0530

    removed unused api

--- index.js
+++ index.js
@@ -19,9 +19,6 @@ app.get('/', (request, response) => {
 app.get('/sales', db.getSales)
 app.get('/sales/:keyword', db.getSalesKeyword)
 app.put('/add-sales', db.createSales)
-// app.get('/users/:id', db.getSalesById)
-// app.put('/users/:id', db.updateSales)
-// app.delete('/users/:id', db.deleteSales)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
